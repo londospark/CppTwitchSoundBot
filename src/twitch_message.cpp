9b57742c8a188cd0c4181c6001b72c0398bc4f6e
Gareth Hubball <ridecar2@gmail.com>
2019-07-08 01:21:48 +0100

    Removed boost dependencies.

--- src/twitch_message.cpp
+++ src/twitch_message.cpp
@@ -1,64 +1,28 @@
-#include <boost/spirit/include/qi.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/fusion/sequence/intrinsic/at_c.hpp>
-
 #include "twitch_message.h"
 
-namespace qi = boost::spirit::qi;
-namespace ascii = boost::spirit::ascii;
-namespace fusion = boost::fusion;
-
-std::string to_string(std::vector<char> const & vec)
-{
-	return std::string(vec.begin(), vec.end());
-}
+#include <regex>
 
 namespace gh {
 
 	std::optional<twitch_message>
 		twitch_message::parse(std::string const& message) {
-		using ascii::char_;
 
 		twitch_message msg;
 
-		const auto attrs = [&msg](auto attributes) -> void {
-			msg.user = twitch_user::from_attributes(to_string(attributes));
-		};
-
-		const auto user = [&msg](auto username) -> void {
-			msg.username = to_string(username);
-		};
-
-		const auto msgType = [&msg](auto type) -> void {
-			msg.message_type = to_string(type);
-		};
-
-		const auto channel = [&msg](auto channel) -> void {
-			msg.channel = to_string(channel);
-		};
-
-		const auto body = [&msg](auto body) -> void {
-			msg.body = to_string(body);
-		};
-
 		// @key=value;key=value :rhymu8354!rhymu8354@rhymu8354.tmi.twitch.tv PRIVMSG #garethhubball :test
-		//std::regex re(R"RE(^@(.+) :([^!]+)![^@]+@[^\.]+\.tmi\.twitch\.tv ([^\s]+) #([^\s]+) :(.*))RE");
-		auto messageParser =
-			boost::proto::deep_copy(
-				'@'
-				>> (+(char_ - ' '))[attrs] >> " :"
-				>> (+(char_ - '!'))[user]
-				>> (+(char_ - '@')) >> '@'
-				>> (+(char_ - '.')) >> '.'
-				>> "tmi.twitch.tv "
-				>> (+(char_ - ' '))[msgType] >> " #"
-				>> (+(char_ - ' '))[channel] >> " :"
-				>> (+(char_ - '\r'))[body] >> "\r\n");
+		std::regex re(R"RE(^@(.+) :([^!]+)![^@]+@[^\.]+\.tmi\.twitch\.tv ([^\s]+) #([^\s]+) :(.*))RE");
+		std::smatch match;
 
-		if (qi::parse(message.begin(), message.end(), messageParser))
-		{
+		if (std::regex_search(message, match, re)) {
+			msg.user = twitch_user::from_attributes(match[1]);
+			msg.message_type = match[3];
+			msg.channel = match[4];
+			msg.body = match[5];
+
 			return msg;
 		}
-		return {};
+		else {
+			return {};
+		}
 	}
 }
